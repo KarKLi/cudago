8f4ce21443c7508d53f3868c3e924eafa342f714
KarKLi <kark_li@qq.com>
2022-02-20 13:18:37 +0800

    feat: add new function and licensee from NVIDIA

--- windows/cuda_runtime.go
+++ windows/cuda_runtime.go
@@ -1,3 +1,53 @@
+/*
+ * Copyright 1993-2018 NVIDIA Corporation.  All rights reserved.
+ *
+ * NOTICE TO LICENSEE:
+ *
+ * This source code and/or documentation ("Licensed Deliverables") are
+ * subject to NVIDIA intellectual property rights under U.S. and
+ * international Copyright laws.
+ *
+ * These Licensed Deliverables contained herein is PROPRIETARY and
+ * CONFIDENTIAL to NVIDIA and is being provided under the terms and
+ * conditions of a form of NVIDIA software license agreement by and
+ * between NVIDIA and Licensee ("License Agreement") or electronically
+ * accepted by Licensee.  Notwithstanding any terms or conditions to
+ * the contrary in the License Agreement, reproduction or disclosure
+ * of the Licensed Deliverables to any third party without the express
+ * written consent of NVIDIA is prohibited.
+ *
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, NVIDIA MAKES NO REPRESENTATION ABOUT THE
+ * SUITABILITY OF THESE LICENSED DELIVERABLES FOR ANY PURPOSE.  IT IS
+ * PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF ANY KIND.
+ * NVIDIA DISCLAIMS ALL WARRANTIES WITH REGARD TO THESE LICENSED
+ * DELIVERABLES, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THESE LICENSED DELIVERABLES.
+ *
+ * U.S. Government End Users.  These Licensed Deliverables are a
+ * "commercial item" as that term is defined at 48 C.F.R. 2.101 (OCT
+ * 1995), consisting of "commercial computer software" and "commercial
+ * computer software documentation" as such terms are used in 48
+ * C.F.R. 12.212 (SEPT 1995) and is provided to the U.S. Government
+ * only as a commercial end item.  Consistent with 48 C.F.R.12.212 and
+ * 48 C.F.R. 227.7202-1 through 227.7202-4 (JUNE 1995), all
+ * U.S. Government End Users acquire the Licensed Deliverables with
+ * only those rights set forth herein.
+ *
+ * Any use of the Licensed Deliverables in individual and commercial
+ * software must include, in the user documentation and internal
+ * comments to the code, the above Disclaimer and U.S. Government End
+ * Users Notice.
+ */
+
+// package windows implements Cuda C Runtime library to Go.
 package windows
 
 import (
@@ -49,6 +99,20 @@ func NewCudaRTLib(d *syscall.DLL) *CudaRTLib {
 	}
 }
 
+// CudaFree - CUDA library host function
+//
+// cudaError_t cudaDeviceReset(void);
+func (l *CudaRTLib) CudaDeviceReset() error {
+	r1, err := callCUDAFuncRetInt(l.d, "cudaDeviceReset")
+	if err != nil {
+		return err
+	}
+	if r1 != 0 {
+		return fmt.Errorf("cudaDeviceReset error: %v", cudaErrorHandler(l, CUDAError_t(r1)))
+	}
+	return nil
+}
+
 // CudaDeviceSynchronize - CUDA library host function
 //
 // cudaError_t cudaDeviceSynchronize(void);
@@ -68,6 +132,34 @@ func (l *CudaRTLib) CudaDeviceSynchronize() error {
 // cudaError_t cudaDeviceSetLimit(enum cudaLimit limit, size_t value);
 func (l *CudaRTLib) CudaDeviceSetLimit(limit cudago.CudaLimit, value uint64) error {
 	r1, err := callCUDAFuncRetInt(l.d, "cudaDeviceSetLimit", uintptr(limit), uintptr(value))
+	if err != nil {
+		return err
+	}
+	if r1 != 0 {
+		return fmt.Errorf("cudaDeviceSetLimit error: %v", cudaErrorHandler(l, CUDAError_t(r1)))
+	}
+	return nil
+}
+
+// CudaDeviceGetLimit - CUDA library host function
+//
+// cudaError_t cudaDeviceGetLimit(size_t *pValue, enum cudaLimit limit);
+func (l *CudaRTLib) CudaDeviceGetLimit(pValue *uint64, limit cudago.CudaLimit) error {
+	r1, err := callCUDAFuncRetInt(l.d, "cudaDeviceGetLimit", uintptr(unsafe.Pointer(pValue)), uintptr(limit))
+	if err != nil {
+		return err
+	}
+	if r1 != 0 {
+		return fmt.Errorf("cudaDeviceSetLimit error: %v", cudaErrorHandler(l, CUDAError_t(r1)))
+	}
+	return nil
+}
+
+// CudaDeviceGetCacheConfig - CUDA library host function
+//
+// cudaError_t cudaDeviceGetCacheConfig(enum cudaFuncCache *pCacheConfig);
+func (l *CudaRTLib) CudaDeviceGetCacheConfig(pCacheConfig *cudago.CudaFuncCache) error {
+	r1, err := callCUDAFuncRetInt(l.d, "cudaDeviceGetCacheConfig", uintptr(unsafe.Pointer(pCacheConfig)))
 	if err != nil {
 		return err
 	}
@@ -115,20 +207,6 @@ func (l *CudaRTLib) CudaFree(devPtr VoidPtr) error {
 	}
 	if r1 != 0 {
 		return fmt.Errorf("cudaFree error: %v", cudaErrorHandler(l, CUDAError_t(r1)))
-	}
-	return nil
-}
-
-// CudaFree - CUDA library host function
-//
-// cudaError_t cudaDeviceReset(void);
-func (l *CudaRTLib) CudaDeviceReset() error {
-	r1, err := callCUDAFuncRetInt(l.d, "cudaDeviceReset")
-	if err != nil {
-		return err
-	}
-	if r1 != 0 {
-		return fmt.Errorf("cudaDeviceReset error: %v", cudaErrorHandler(l, CUDAError_t(r1)))
 	}
 	return nil
 }
